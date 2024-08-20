5281e8c25547f69a372b2b95c9f7b5942b353bb0
JosueRFP <josue.petri.senac@gmail.com>
2024-08-20 14:29:22 -0300

    Pause

--- Arkonoid/Assets/Script/Pause.cs
+++ Arkonoid/Assets/Script/Pause.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Pause : MonoBehaviour
 {
+    public UnityEvent OnPause;
+    public UnityEvent OnUnPause;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,17 @@ public class Pause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (Time.timeScale == 0)
+            {
+                //Despause
+                Time.timeScale = 1;
+                OnUnPause.Invoke();
+                //Pause
+                Time.timeScale = 0;
+                OnPause.Invoke();
+            }
+        }
     }
 }
