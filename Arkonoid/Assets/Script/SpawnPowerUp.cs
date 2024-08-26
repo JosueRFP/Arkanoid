a5ad2a1b60086095a8131b9ee9345a115fc9d924
JosueRFP <josue.petri.senac@gmail.com>
2024-08-26 14:36:14 -0300

    Spawn power ups ainda não funcionante

---
+++ Arkonoid/Assets/Script/SpawnPowerUp.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPowerUp : MonoBehaviour
+{
+    public GameObject[] powerUps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void PowerUpsInstance()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Instantiate(powerUps[i]);
+        }
+    }
+}
