79de76675e2af5033b746808e1c0f6d2e5e712d3
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2024-08-26 14:40:21 -0300

    codigo

--- Arkonoid/Assets/Script/StarGame.cs
+++ Arkonoid/Assets/Script/StarGame.cs
@@ -4,33 +4,46 @@ using UnityEngine;
 using UnityEngine.UI;
 public class StarGame : MonoBehaviour
 {
-    public Text countdownText; 
+    public Text countdownText;
     public GameObject player;
+    public GameObject bola;
+    private bool gamePaused;
+
     void Start()
     {
-     
-        player.SetActive(false);
-
        
+        player.SetActive(false);
+        bola.SetActive(false);
+        gamePaused = true;
+
+        
         StartCoroutine(Countdown());
     }
 
     IEnumerator Countdown()
     {
-        int countdownTime = 3; 
+        int countdownTime = 3;
 
-       
         while (countdownTime > 0)
         {
             countdownText.text = countdownTime.ToString();
-            yield return new WaitForSeconds(1f); 
-            countdownTime--; 
+            yield return new WaitForSeconds(1f);  
+            countdownTime--;
         }
 
-        
         countdownText.text = "Go!";
         yield return new WaitForSeconds(1f);
-        countdownText.gameObject.SetActive(false); 
-        player.SetActive(true); 
+
+      
+        countdownText.gameObject.SetActive(false);
+        player.SetActive(true);
+        bola.SetActive(true);
+        gamePaused = false;
+    }
+
+    void Update()
+    {
+     
+
     }
 }
