9f5c8fe7a9a341c6f04f6dce7d3c56672619708e
JosueRFP <josue.petri.senac@gmail.com>
2024-08-27 14:34:06 -0300

    Um power up feito

---
+++ Arkonoid/Assets/Script/PowerUpScipt.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpScipt : MonoBehaviour
+{
+    public int multiplier = 3;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(new Vector2(0f, -1f) * Time.deltaTime * speed   );
+    }
+     void OnTriggerEnter2D(Collider2D other)
+     {
+        if (other.CompareTag("Bola"))
+        {
+            Pickup(other);
+        }
+     }
+    //tudo que está em verde é o tipo.
+    void Pickup(Collider2D bola)
+    {
+        Debug.Log("Pao de cada dia!");
+        bola.transform.localScale *= multiplier;
+        Destroy(gameObject);
+    }
+}// fonte: https://www.youtube.com/watch?v=CLSiRf_OrBk
