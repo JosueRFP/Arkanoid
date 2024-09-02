98613377a5c4ec4eed487990ac0916efe0732695
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2024-09-02 14:10:08 -0300

    configurando codigo

--- Arkonoid/Assets/Script/SpawnTijolo.cs
+++ Arkonoid/Assets/Script/SpawnTijolo.cs
@@ -11,34 +11,59 @@ public class SpawnTijolo : MonoBehaviour
     public float verticalSpacing = 1.5f;
     public Vector2 startPosition = new Vector2(-7f, 0f);
 
+    private List<GameObject> tijolosAtivos = new List<GameObject>();
+
     void Start()
     {
-        SpawnRandomRows(rowCount, tijolosPorFileira, startPosition); 
+        SpawnRandomRows(rowCount, tijolosPorFileira, startPosition);
+    }
 
-
-        void SpawnRandomRows(int rowCount, int tijolosPorFileira, Vector2 startPosition)
+    void SpawnRandomRows(int rowCount, int tijolosPorFileira, Vector2 startPosition)
+    {
+        for (int row = 0; row < rowCount; row++)
         {
-            for (int row = 0; row < rowCount; row++)
-            {
-             
-                Vector2 rowStartPosition = new Vector2(startPosition.x, startPosition.y - row * verticalSpacing);
-
-              
-                SpawnRandomRow(tijolosPorFileira, rowStartPosition);
-            }
+            Vector2 rowStartPosition = new Vector2(startPosition.x, startPosition.y - row * verticalSpacing);
+            SpawnRandomRow(tijolosPorFileira, rowStartPosition);
         }
+    }
 
-        void SpawnRandomRow(int tijolosPorFileira, Vector2 startPosition)
+    void SpawnRandomRow(int tijolosPorFileira, Vector2 startPosition)
+    {
+        float px = startPosition.x;
+
+        for (int i = 0; i < tijolosPorFileira; i++)
         {
-            float px = startPosition.x;
-
-            for (int i = 0; i < tijolosPorFileira; i++)
-            {
-                int random = Random.Range(0, blocos.Length); 
-                Vector2 spawnPosition = new Vector2(px, startPosition.y); 
-                Instantiate(blocos[random], spawnPosition, Quaternion.identity); 
-                px += horizontalSpacing; 
-            }
+            int random = Random.Range(0, blocos.Length);
+            Vector2 spawnPosition = new Vector2(px, startPosition.y);
+            GameObject tijolo = Instantiate(blocos[random], spawnPosition, Quaternion.identity);
+            tijolosAtivos.Add(tijolo); 
+            tijolo.GetComponent<Tijolo>().SetSpawner(this); 
+            px += horizontalSpacing;
         }
+    }
+
+    public void RespawnTijolo(GameObject tijolo)
+    {
+        StartCoroutine(RespawnTijoloCoroutine(tijolo));
+    }
+
+    IEnumerator RespawnTijoloCoroutine(GameObject tijolo)
+    {
+        
+        Vector2 spawnPosition = tijolo.transform.position;
+
+      
+        tijolosAtivos.Remove(tijolo);
+        Destroy(tijolo);
+
+        
+        yield return new WaitForSeconds(2);
+
+       
+        int random = Random.Range(0, blocos.Length);
+        GameObject novoTijolo = Instantiate(blocos[random], spawnPosition, Quaternion.identity);
+
+        tijolosAtivos.Add(novoTijolo);
+        novoTijolo.GetComponent<Tijolo>().SetSpawner(this);
     }
 }
--- Arkonoid/Assets/Script/Tijolo.cs
+++ Arkonoid/Assets/Script/Tijolo.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 
 public class Tijolo : MonoBehaviour
 {
-   
+    private SpawnTijolo spawner;
+
+    public void SetSpawner(SpawnTijolo spawner)
+    {
+        this.spawner = spawner;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bola"))
@@ -13,16 +19,22 @@ public class Tijolo : MonoBehaviour
             Die();
         }
     }
+
     private void Die()
     {
-
+       
         Player player = FindObjectOfType<Player>();
         if (player != null)
         {
             player.AddScore(10);
         }
 
+        if (spawner != null)
+        {
+            spawner.RespawnTijolo(gameObject);
+        }
+
+        
         Destroy(gameObject);
     }
-
 }
